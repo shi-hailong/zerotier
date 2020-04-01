53315665cfffeddca883feb39caa6abd8c5c3238
shi-hailong <ushihl@163.com>
2020-04-01 13:44:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-mkdir -p /var/lib/zerotier-one
+
+export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+
+if [ ! -e /dev/net/tun ]; then
+	echo 'FATAL: cannot start ZeroTier One in container: /dev/net/tun not present.'
+	exit 1
+fi
+
 zerotier-one -d
 
 if [ -n "$NW_ID" ]; then
